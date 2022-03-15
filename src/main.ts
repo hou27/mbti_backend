fa2ef259c7d8c8a08ac4a5dc4a96604bca09d323
hou27 <ataj125@gmail.com>
2022-03-16 01:18:04 +0900

    Listen heroku port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // app.use(JwtMiddleware); // only for functional middleware
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
